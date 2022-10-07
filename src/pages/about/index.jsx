6cc7ee41ceba5e8de437e1ff87beda4780bf91e9
Wata Negreiros <90472705+WataNegreirosMonteiro@users.noreply.github.com>
2022-10-07 20:05:50 +0000

    adicionado texto do about

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,8 +2,11 @@ export default function About(){
   return(
     <div className="about-container">
       <nav>
-      </nav>
+      </nav> 
       <section>
+        <p className="about-text">
+          O aplicativo tem como objetivo auxiliar os alunos do Instituto Federal de Educação, Ciência e Tecnologia de Rondônia (IFRO), por meio de um gerenciador de tarefas escolares prático, desenvolvido por alunos do instituto.
+        </p>
       </section>
     </div>
   )
