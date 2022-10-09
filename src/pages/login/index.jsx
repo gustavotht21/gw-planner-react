062d8d6c74e4846750e3803b2da3f47f90fba7a7
Wata Negreiros <90472705+WataNegreirosMonteiro@users.noreply.github.com>
2022-10-09 13:59:20 +0000

    resolvendo o conflito do login area

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,7 @@
 export default function Login(){
   return(
     <>
-      <h3>
-        Identificação
-      </h3>
-      <p>
-        Identifique-se para acessar informações pessoais
-      </p>
+      login area
     </>
   )
 }
