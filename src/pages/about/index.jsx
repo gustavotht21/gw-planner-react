0d77ad9f1ec4fe79bf62200e255edbc541afcb22
Wata Negreiros <90472705+WataNegreirosMonteiro@users.noreply.github.com>
2022-10-09 17:16:49 +0000

    Página sobre - sem o navbar

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,9 @@
+import "./style.css"
+  
 export default function About(){
   return(
     <div className="about-container">
-      <nav>
-      </nav> 
-      <section>
+      <section className="about-section">
         <p className="about-text">
           O aplicativo tem como objetivo auxiliar os alunos do Instituto Federal de Educação, Ciência e Tecnologia de Rondônia (IFRO), por meio de um gerenciador de tarefas escolares prático, desenvolvido por alunos do instituto.
         </p>
