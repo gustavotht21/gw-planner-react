b311bb275c06286a70b7ff11822c4e009f41f6c2
Gustavo Casagrande Borges <113564851+gustavotht21@users.noreply.github.com>
2022-10-09 16:18:23 +0000

    Adicionado a página de login para mobile

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,14 +1,26 @@
+import React from 'react'
+
+
+import "./style.css"
+
 export default function Login(){
   return(
     <>
-      <header>
-        <h3>
-          Identificação
-        </h3>
-
-        
-      
+      <header className="header-container">
+        <div className="header-text">
+          <h3 className="header-title">
+            Identificação
+          </h3>
+          <p className="header-paragraph">
+            Identifique-se para acessar informações pessoais
+          </p>
+        </div>
       </header>
+      <div className="login-container">
+        {/* Colocar o botão de matrícula e entrar como componentes aqui */}
+        <input type='text' className="input-registration" placeholder="Matrícula"></input>
+        <button className="login-button">Entrar</button>
+      </div>
     </>
   )
 }
